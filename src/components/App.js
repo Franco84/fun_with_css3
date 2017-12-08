a53044982128ebfae8d8642d86eeee249617acfe
franco83-2 <franco.84@gmail.com>
2017-12-08 13:52:14 -0800

    created header

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react'
+import {Grid, Row, Col} from 'react-bootstrap'
+import Header from './Header'
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        Hi!
-      </div>
+      <Grid>
+        <Header />
+        <Row>
+          <Col>
+            
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
